9b05f3659323272f597120e904a93599c9f92e99
kristaponis <a.kristaponis@icloud.com>
2022-02-18 11:40:40 +0200

    Add Bubble Sort

---
+++ bubble-sort/bubble-sort.js
@@ -0,0 +1,15 @@
+// Bubble sort
+let numArr = [2, 1, 6, 4, 8, 9, 3, 10, 5, 7]
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr
+}
