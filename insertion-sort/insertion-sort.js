612d24de55ea90c4f93c8c7f06a09cb0b3af198e
kristaponis <a.kristaponis@icloud.com>
2022-02-20 10:47:24 +0200

    Add Insertion Sort

---
+++ insertion-sort/insertion-sort.js
@@ -0,0 +1,17 @@
+// Some array with only sorted beginning
+let arr = [1, 2, 7, 5, 12, 84, 9, 16, 17]
+console.log(arr);
+
+function insertionSort(n) {
+    for (let i = 1; i < n.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (n[i] < n[j]) {
+                let spl = n.splice(i, 1)
+                n.splice(j, 0, spl[0])
+            }
+        }
+    }
+    return n
+}
+
+console.log(insertionSort(arr));
