5db8e580742ffaddbbd2ed3ef7333aa71ef2a1b2
kristaponis <a.kristaponis@icloud.com>
2022-04-03 18:07:04 +0300

    Bubble Sort update

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -1,6 +1,3 @@
-// Bubble sort
-let numArr = [2, 1, 6, 4, 8, 9, 3, 10, 5, 7]
-
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
